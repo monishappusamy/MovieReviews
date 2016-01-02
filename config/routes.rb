6b0e5e31f164b92434f0738d6c6d08c83c22fe58
monish appusamy <monishappusamy@gmail.com>
2016-01-02 17:05:42 -0500

    linked reviews with movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :reviews
   devise_for :users
-  resources :movies
-  get 'welcome/index'
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
   root 'movies#index'
 end
